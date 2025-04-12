ad78aa3010df077a4b24fd2fe4a6e2215200e61e
Santiago Mapura h <smapura107592@umanizales.edu.co>
2025-04-11 20:13:54 -0500

    1 Y 2 cohorte

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from Controler.abb_controler import abb_route
+
+app = FastAPI()
+
+# Incluir el router con el prefijo /abb ya definido
+app.include_router(abb_route)
+
+# Puedes agregar una ruta raíz opcional para verificar el estado del API
+@app.get("/")
+def read_root():
+    return {"message": "API de mascotas con ABB funcionando"}
---
+++ model/pet.py
@@ -0,0 +1,8 @@
+from pydantic import BaseModel
+
+class Pet(BaseModel):
+    id:int
+    name:str
+    age: int
+    breed: str
+    location: str
